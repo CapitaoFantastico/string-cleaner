4eacb3da35d6272e15ad7e4923dd00bd93fbeb3d
Henrique Souza <henriquesouza182@gmail.com>
2019-06-10 15:50:12 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const $ = selector => document.querySelector(selector);
+
+$("#format").addEventListener("keyup", e => {
+  const value = $("#format").value;
+  if (validText(value)) {
+    $("#result").value = trimText(convertToOneLine(value));
+  } else {
+    $("#result").value = "";
+  }
+});
+
+const validText = text => {
+  if (!text) {
+    return false;
+  }
+  if (typeof text !== "string") {
+    return false;
+  }
+  return true;
+};
+
+const trimText = text => {
+  return text.replace(/^\s+|\s+$/g, "").trim();
+};
+
+const convertToOneLine = text => {
+  return text.replace(/\s+/g, " ").trim();
+};
